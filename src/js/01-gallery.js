c09f2df17c2d7a6f9140d9cfe072b68170bc8154
OlgaKhlivnenko <xlivnenko@ukr.net>
2021-11-25 14:02:40 -0500

    26-2-11

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector(`.gallery`);
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML(`beforeend`, cardsMarkup);
 
+
+function createImageCardsMarkup(items) {
+    return items
+        .map(({ preview, original, description}) => {
+            return `
+              <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+                  `;
+        })
+        .join(``);  
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+    });
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import VideoPlayer from './Video.Player'
 
+function (Player) {
+        const iframe = document.querySelector('iframe');
+        const player = new Player(iframe);
+
+        player.on('play', function() {
+            console.log('played the video!');
+        });
+    });
